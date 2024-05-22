334469f983d194306bdfc75149002e05f88e1d94
reyesjl <reyesjl.nine@hotmail.com>
2024-05-21 23:13:46 -0400

    No Story - Create core app
    * add core app for site home and general
    * create index view and url
    * add url to settings

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("core.urls")),
     path("events/", include("events.urls")),
     path("admin/", admin.site.urls),
 ]
