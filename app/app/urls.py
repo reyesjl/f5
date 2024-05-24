682ee9d1eb98e9af8619ec8d82f82c7314b96db6
reyesjl <reyesjl.nine@hotmail.com>
2024-05-24 12:23:03 -0400

    Issue 009 : add event custom descriptions
    - add ckeditor5
    - display content on details page

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("core.urls")),
     path("events/", include("events.urls")),
     path("clubs/", include("clubs.urls")),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
