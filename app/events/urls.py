76e5dc5b7129d657b4070b44042322e7dc19813e
Jose <reyesjl.nine@hotmail.com>
2024-05-24 17:44:53 -0400

    Issue 5 : add ability to rsvp for event (#17)
    - add rsvp ability
    - add check for duplicate rsvp to same event
    - add rsvp rsvp receipt with updating details
    - add decorator for invalid receipt uuid
    - add form for rsvp with styles
    - add filter snippet to use on filter for event
    - add token for receipts

--- app/events/urls.py
+++ app/events/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('type/<str:event_type>/', views.events_by_type, name='events_by_type'),
     path('details/<int:event_id>/', views.event_details, name='event_details'),
+    path('details/<int:event_id>/rsvp/', views.rsvp_create, name='rsvp_create'),
+    path('rsvp/<str:token>/receipt/', views.rsvp_receipt, name='rsvp_receipt'),
 ]
