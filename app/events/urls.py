5450d8a38aeac138839f09e4569a34394dcc85d3
Jose <reyesjl.nine@hotmail.com>
2024-05-24 21:32:26 -0400

    Issue 08 : create reminder system (#18)
    - add basic pdf donwload
    - add copy link
    - add expose receipt token

--- app/events/urls.py
+++ app/events/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('details/<int:event_id>/', views.event_details, name='event_details'),
     path('details/<int:event_id>/rsvp/', views.rsvp_create, name='rsvp_create'),
     path('rsvp/<str:token>/receipt/', views.rsvp_receipt, name='rsvp_receipt'),
+    path('rsvp/<str:token>/generate-pdf/', views.generate_pdf, name='generate_pdf'),
 ]
