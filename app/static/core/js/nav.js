d8f0d47a89fda83d2b4b267c155695f30a91478e
reyesjl <reyesjl.nine@hotmail.com>
2024-05-31 16:12:48 -0400

    Issue 27 : add notifications
    - add notification styles
    - send message as notifcation to home for testing
    - add notifcations to nav.js for dismiss func
    - add health filters; not in use yet.
    - add logo to home;
    - make tagline shorter;

--- app/static/core/js/nav.js
+++ app/static/core/js/nav.js
@@ -1,6 +1,5 @@
 const nav = document.querySelector(".navlist");
 const navToggle = document.querySelector(".mobile-nav-toggle");
-
 navToggle.addEventListener('click', () => {
     const visibility = nav.getAttribute("data-visible");
     
@@ -11,4 +10,16 @@ navToggle.addEventListener('click', () => {
         nav.setAttribute("data-visible", false);
         navToggle.setAttribute("aria-expanded", false);
     }
+});
+
+const alerts = document.querySelectorAll('.alert');
+const dismissButtons = document.querySelectorAll('.alertdismiss');
+dismissButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        const alert = e.target.closest('.alert');
+        alert.classList.add('fade-out');
+        alert.addEventListener('animationend', () => {
+            alert.remove();
+        });
+    });
 });
