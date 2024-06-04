8bf01ed934601818405de9505936a9c9aaabc1cb
reyesjl <reyesjl.nine@hotmail.com>
2024-06-04 18:57:33 -0400

    - add urls for posts - add query set for posts - add manager for posts - add CRUD views for posts - add form for posts - add user route security

--- app/health/models.py
+++ app/health/models.py
@@ -4,6 +4,52 @@ from django.utils import timezone
 from django_ckeditor_5.fields import CKEditor5Field
 
 
+class PostQuerySet(models.QuerySet):
+    def published(self):
+        return self.filter(status='published')
+
+    def drafts(self):
+        return self.filter(status='draft')
+    
+    def featured(self):
+        return self.filter(featured=True)
+    
+    def by_slug(self, slug):
+        return self.get(slug=slug)
+    
+    def by_visibility(self, visibility):
+        if visibility == 'published':
+            return self.filter(status='published')
+        elif visibility == 'draft':
+            return self.filter(status='draft')
+        return self
+    
+    def by_tag(self, tag):
+        return self.filter(tags__icontains=tag)
+    
+class PostManager(models.Manager):
+    def get_queryset(self):
+        return PostQuerySet(self.model, using=self._db)
+
+    def published(self):
+        return self.get_queryset().published()
+
+    def drafts(self):
+        return self.get_queryset().drafts()
+    
+    def featured(self):
+        return self.get_queryset().featured()
+    
+    def by_slug(self, slug):
+        return self.get_queryset().by_slug(slug)
+    
+    def by_visibility(self, visibility):
+        return self.get_queryset().by_visibility(visibility)
+    
+    def by_tag(self, tag):
+        return self.get_queryset().by_tag(tag)
+    
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -14,6 +60,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     #author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='plans')
     author = models.CharField(max_length=50, blank=True)
+    featured = models.BooleanField(default=False)
     excerpt = models.TextField(max_length=300, blank=True)
     content = CKEditor5Field('Text', config_name='extends', default=None, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -25,6 +72,8 @@ class Post(models.Model):
     views = models.PositiveIntegerField(default=0)
     likes = models.PositiveIntegerField(default=0)
 
+    objects = PostManager()
+
     class Meta:
         ordering = ['-created_at']
 
@@ -35,6 +84,10 @@ class Post(models.Model):
             date_str = self.created_at.strftime("%Y-%m-%d")
             self.slug = slugify(f"{self.title}-{date_str}")
         super().save(*args, **kwargs)
+
+    def add_view(self):
+        self.views += 1
+        self.save(update_fields=['views'])
 
     def __str__(self):
         return self.title
--- app/health/views.py
+++ app/health/views.py
@@ -1,4 +1,108 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
+from .models import Post
+from .forms import PostForm
+from core.utils import check_user
+from core.decorator import user_has_role
+
 
 def index(request):
-    return render(request, "health/index.html")
+    posts = Post.objects.published()
+    return render(request, "health/index.html", {"posts": posts})
+
+def post_list(request):
+    posts = Post.objects.published()
+    can_manage = check_user(request.user, "health_manager")
+
+    # Filter on post visibility
+    post_visibility = request.GET.get("post_visibility")
+    if post_visibility and can_manage:
+        posts = Post.objects.all()
+        posts = posts.by_visibility(post_visibility)
+
+    # Filter on post status    
+    post_status = request.GET.get("post_status")
+    if post_status == "featured":
+        posts = posts.featured()
+
+    # Filter on post focus (tags)
+    post_focus = request.GET.get("post_focus")
+    if post_focus:
+        if post_focus != 'all':
+            posts = posts.by_tag(post_focus)
+
+    context = {
+        "posts": posts,
+        "can_manage": can_manage,
+    }
+    return render(request, "posts/post_list.html", context)
+
+@user_has_role("health_manager")
+def post_create(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("post-list"))
+    else:
+        form = PostForm()
+
+    return render(request, "posts/post_create.html", {"form": form})
+
+def post_detail(request, post_slug):
+    try:
+        post = Post.objects.by_slug(slug=post_slug)
+    except Exception as e:
+        message = e
+        return render(request, "posts/post_error.html", {"message": message})
+
+    # update post views
+    post_id = post.id
+    viewed_posts = request.session.get("viewed_posts", [])
+    if post_id not in viewed_posts:
+        # Update post views and add post ID to viewed_posts
+        post.add_view()
+        viewed_posts.append(post_id)  # Convert to list
+        request.session["viewed_posts"] = viewed_posts
+    else: 
+        print(viewed_posts)
+
+    can_manage = check_user(request.user, "health_manager")
+
+    context = {
+        "post": post,
+        "can_manage": can_manage,
+    }
+    return render(request, "posts/post_detail.html", context)
+
+@user_has_role("health_manager")
+def post_update(request, post_slug):
+    try:
+        post = Post.objects.by_slug(slug=post_slug)
+    except Exception as e:
+        message = e
+        return render(request, "posts/post_error.html", {"message": message})
+    
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse("post-list"))
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, "posts/post_update.html", {"form": form})
+
+@user_has_role("health_manager")
+def post_delete(request, post_slug):
+    try:
+        post = Post.objects.by_slug(slug=post_slug)
+    except Exception as e:
+        message = e
+        return render(request, "posts/post_error.html", {"message": message})
+    
+    if request.method == "POST":
+        post.delete()
+        return redirect("post-list")
+    
+    return render(request, "posts/post_delete_confirm.html", {"post": post})
