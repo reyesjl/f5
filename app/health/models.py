121f95df985226125bfab339ecb1de5e0a12b518
reyesjl <reyesjl.nine@hotmail.com>
2024-06-04 09:46:49 -0400

    Issue 29 : build health app
    - add post model to health app
    - add model to admin
    - make migrations

--- app/health/models.py
+++ app/health/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils import timezone
+from django_ckeditor_5.fields import CKEditor5Field
 
-# Create your models here.
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    #author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='plans')
+    author = models.CharField(max_length=50, blank=True)
+    excerpt = models.TextField(max_length=300, blank=True)
+    content = CKEditor5Field('Text', config_name='extends', default=None, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.CharField(max_length=200, blank=True)
+    featured_image = models.ImageField(upload_to='featured_images/', blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    reading_time = models.IntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+    likes = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            if not self.created_at:
+                self.created_at = timezone.now()
+            date_str = self.created_at.strftime("%Y-%m-%d")
+            self.slug = slugify(f"{self.title}-{date_str}")
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
