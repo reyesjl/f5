167bf73167b3964e2564df98adb294e585df242c
reyesjl <reyesjl.nine@hotmail.com>
2024-06-04 18:56:22 -0400

    - add featured + published posts to home page

--- app/core/views.py
+++ app/core/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from events.models import Event
+from health.models import Post
 from django.contrib import messages
 
 def index(request):
-    events = Event.objects.all()
-    events = events.upcoming().featured()[:5]
+    events = Event.objects.upcoming().featured()[:5]
+    posts = Post.objects.published().featured()[:5]
 
     # messages.add_message(request, messages.INFO, 'Welcome to First Five Rugby!', extra_tags='info')
     # messages.add_message(request, messages.SUCCESS, 'Your operation was successful.', extra_tags='success')
@@ -14,5 +15,6 @@ def index(request):
 
     context = {
         "events": events,
+        "posts": posts,
     }
     return render(request, 'core/index.html', context)
