038ee8d3959971037d7512c588d6f90f22758e3e
reyesjl <reyesjl.nine@hotmail.com>
2024-07-27 23:49:28 -0400

    update permissions;

--- app/blog/views.py
+++ app/blog/views.py
@@ -2,21 +2,19 @@ from django.shortcuts import redirect, render
 from django.contrib import messages
 from .forms import ArticleForm
 from core.utils import check_user, get_object_or_error
-from core.decorator import user_has_role
+from core.decorator import user_has_role, is_staff, is_trainer, is_staff_or_trainer
 from .models import Article
 
 def index(request):
     articles = Article.objects.published()
-    can_manage = check_user(request.user, "blog_manager")
 
     context = {
         "articles": articles,
-        "can_manage": can_manage,
     }
 
     return render(request, "articles/index.html", context)
 
-@user_has_role("blog_manager")
+@is_staff_or_trainer
 def article_create(request):
     if request.method == "POST":
         form = ArticleForm(request.POST)
@@ -32,27 +30,15 @@ def article_create(request):
     return render(request, "articles/article_form.html", {"form": form})
 
 def article_detail(request, slug):
-    can_manage = check_user(request.user, "blog_manager")
-    try:
-        article = Article.objects.by_slug(slug=slug)
-    except Article.DoesNotExist:
-        messages.error(request, f"The requested article does not exists.", extra_tags="error")
-    except Exception as e:
-        messages.error(request, f"An unexpected error occured: {e}", extra_tags="error")
-        return render(request, "core/error.html")
-    
-    if not can_manage and article.visibility == 'draft':
-        messages.error(request, f"This article is not published.", extra_tags="error")
-        return render(request, "core/error.html")
+    article = get_object_or_error(Article, slug=slug)
     
     context = {
         "article": article,
-        "can_manage": can_manage
     }
 
     return render(request, "articles/article_detail.html", context)
 
-@user_has_role("blog_manager")
+@is_staff_or_trainer
 def article_update(request, slug):
     article = get_object_or_error(Article, slug=slug)
     
@@ -69,15 +55,9 @@ def article_update(request, slug):
     
     return render(request, "articles/article_form.html", {"form": form})
 
-@user_has_role("blog_manager")
+@is_staff_or_trainer
 def article_delete(request, slug):
-    try:
-        article = Article.objects.by_slug(slug=slug)
-    except Article.DoesNotExist:
-        messages.error(request, f"The requested article does not exists.", extra_tags="error")
-    except Exception as e:
-        messages.error(request, f"An unexpected error occured: {e}", extra_tags="error")
-        return render(request, "core/error.html")
+    article = get_object_or_error(Article, slug=slug)
     
     if request.method == "POST":
         article.delete()
