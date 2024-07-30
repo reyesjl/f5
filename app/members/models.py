b84a33dfb35ffd8b379714f0773aaa40a025ad6d
reyesjl <reyesjl.nine@hotmail.com>
2024-07-30 17:43:25 -0400

    crop, convert, and resize avatar image on upload;

--- app/members/models.py
+++ app/members/models.py
@@ -1,4 +1,7 @@
+import io
+from PIL import Image
 from django.db import models
+from django.core.files.base import ContentFile
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
@@ -14,13 +17,42 @@ class CustomUser(AbstractUser):
         return self.username
     
     def save(self, *args, **kwargs):
-        try:
-            this = CustomUser.objects.get(id=self.id)
-            if this.avatar != self.avatar:
-                this.avatar.delete(save=False)
-        except CustomUser.DoesNotExist:
-            pass  # This means the user is being created, not updated
+        if self.avatar:
+            # Process image if it exists
+            image = Image.open(self.avatar)
+            image = self.crop_to_square(image)
+            
+            # Save processed image to a temporary file
+            temp_file = io.BytesIO()
+            image.save(temp_file, format='JPEG')
+            temp_file.seek(0)
+            self.avatar.save(self.avatar.name, ContentFile(temp_file.read()), save=False)
+        
+        # Continue with the normal save method
         super(CustomUser, self).save(*args, **kwargs)
+
+    def crop_to_square(self, image):
+        # Calc short for calulator
+        width, height = image.size
+        min_side = min(width, height)
+        
+        # Calculate the coordinates for cropping the center square
+        left = (width - min_side) / 2
+        top = (height - min_side) / 2
+        right = (width + min_side) / 2
+        bottom = (height + min_side) / 2
+        
+        # Crop the image to the calculated square
+        image = image.crop((left, top, right, bottom))
+
+        # Resize the image to 250x250 pixels
+        image = image.resize((250, 250), Image.Resampling.LANCZOS)
+        
+        # Convert image to RGB if it's not already in that mode
+        if image.mode in ("RGBA", "P"):
+            image = image.convert("RGB")
+        
+        return image
 
 class Avatar(models.Model):
     name = models.CharField(max_length=100)
